bcf136109353053db74bdb9498c528547853605e
DalkomCandy <gyumin0815@gmail.com>
2022-02-07 10:47:31 +0900

    2022-02-07

---
+++ SkyScanner/input.py
@@ -0,0 +1,4 @@
+import datetime
+today = datetime.datetime.now()
+a = today.strftime('%Y%m%d')
+print(a)
--- SkyScanner/main.py
+++ SkyScanner/main.py
@@ -2,66 +2,47 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 import pandas as pd
-
+import datetime
 # Basic Settings
-URL = 'https://www.skyscanner.co.kr/'
-FROM = '서울'
-TO = '제주'
-DATE = None
+options = Options()
+options.add_experimental_option("detach", True)
 
-def ChromeOptions():
-    global driver
-    options = Options()
-    options.add_experimental_option("detach", True)
+driver = webdriver.Chrome('chromedriver.exe', options=options)
+driver.implicitly_wait(5)
 
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
-    driver.implicitly_wait(5)
+for i in range(1,20):
+    time.sleep(3.8)
+    FROM = 'GMP'
+    TO = 'CJU'
+    today = datetime.datetime.now() 
+    DATE = str(today.year) + str(today.month).zfill(2) + str(today.day + i).zfill(2)
+    URL = f'https://m-flight.naver.com/flights/domestic/{FROM}-{TO}-{DATE}?adult=1&fareType=YC'
+
     driver.get(url = URL)
+
+    time.sleep(20)
+    driver.find_element(By.CLASS_NAME, 'domestic_current__nsjAG').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[5]/div/div[1]/div/div/div/button[1]').click()
+    time.sleep(1)
+    #-----------------------------------------------------------------------------------------------------
+    columns = ['검색 날짜', '출발 날짜', '항공사', '출발시간', '가격']
+    df = pd.DataFrame(columns = columns)
+    airline = driver.find_elements(By.CLASS_NAME, 'airline')
+    departure = driver.find_elements(By.CLASS_NAME, 'time')
+    price = driver.find_elements(By.CLASS_NAME, 'domestic_num__2roTW')
+
+    for a,b,c in zip(airline, departure, price):
+        df = df.append({'검색 날짜' : today.strftime('%Y%m%d'),'출발 날짜': DATE, '항공사' : a.text , '출발시간' : b.text, '가격' : c.text} , ignore_index=True)
     
-ChromeOptions()
-    
-from_location = driver.find_element_by_name('fsc-origin-search')
-from_location.send_keys(Keys.BACK_SPACE)
-time.sleep(0.5)
-from_location.send_keys(FROM)
-time.sleep(1)
+    time.sleep(2)
+    driver.quit()
 
-driver.find_element_by_name('fsc-destination-search').send_keys(TO)
-time.sleep(1)
-
-search_btn = driver.find_elements_by_xpath('//*[@id="flights-search-controls-root"]/div/div/form/div[3]/button')
-
-driver.execute_script("arguments[0].click();", search_btn)
-time.sleep(1)
-
-columns = ['from_time', 'to_time', 'airline', 'way']
-df = pd.DataFrame(columns = columns)
-
-time.sleep(10)
-#----------------------------------------------------------------------------------------
-driver.find_element_by_xpath('//*[@id="app-root"]/div[1]/div/div[2]/div[2]/div[1]/button').click()
-time.sleep(2)
-
-def scroll_down(whileSeconds):
-    import datetime
-    start = datetime.datetime.now()
-    end = start + datetime.timedelta(seconds=whileSeconds)
-    while True:
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        time.sleep(1)
-        if datetime.datetime.now() > end:
-            break
-scroll_down()
-
-from_time = driver.find_elements_by_class_name('BpkText_bpk-text__YWQwM BpkText_bpk-text--base__NDljN')
-to_time = driver.find_elements_by_class_name('BpkText_bpk-text__YWQwM BpkText_bpk-text--base__NDljN')
-airline = driver.find_elements_by_class_name('BpkText_bpk-text__YWQwM BpkText_bpk-text--xs__MWQxY')
-way = driver.find_elements_by_class_name('BpkText_bpk-text__YWQwM BpkText_bpk-text--xs__MWQxY LegInfo_stopsLabelGreen__YWM4M')
-
-for a,b,c,d in zip(from_time, to_time, airline, way):
-    df = df.append(a,b,c,d)
-
-print(df.head())
-    
+print(len(df))
+print(df)
+        
