d5a82c2b70ec8acbb70fe2bfd12b798dad2308b3
DalkomCandy <gyumin0815@gmail.com>
2022-02-18 17:17:43 +0900

    2022-02-18

--- Naver_Flight/ace.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-import pandas as pd
-
-def csv_to_json(DATE):
-    df_csv = pd.read_csv(f'{DATE}.csv', encoding="utf-8")
-    df_csv.drop(['Unnamed: 0'], axis = 1, inplace = True)
-    print(df_csv.head())
-csv_to_json(20220211)
