7a2f34c3f44c9761bb1f103e921420d1dbcd260d
DalkomCandy <gyumin0815@gmail.com>
2022-03-21 14:39:36 +0900

    Django Commit

--- Django/src/products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    title       = models.CharField(max_length=120, ) # max_length is required for charfield
-    description = models.TextField()
-    price       = models.DecimalField(decimal_places=2, max_digits=200) # 소숫점 2자리
-    summary     = models.TextField(blank=True, null=False)
-    featured    = models.BooleanField()
