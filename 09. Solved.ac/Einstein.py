084fb070d88b1d262afeadf502c0b451f74cdc32
DalkomCandy <gyumin0815@gmail.com>
2022-05-25 16:02:21 +0900

    2022-05-25

---
+++ 09. Solved.ac/Einstein.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+home = ['red', 'green', 'yellow', 'ivory', 'blue']
+nation = ['australia', 'italy', 'ukraine', 'norway','japan']
+drink = ['coffee', 'tea', 'orange', 'water', 'milk']
+food = ['apple', 'banana', 'onion', 'cake', 'mushroom']
+animal = ['fox', 'zebra','horse', 'snail', 'dog']
+
+df = pd.DataFrame(columns=[1,2,3,4,5])
+print(df)
